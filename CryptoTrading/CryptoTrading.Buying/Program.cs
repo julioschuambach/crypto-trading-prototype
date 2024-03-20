638aaac6ff6e84cd4ba8de38ab327a43a69253ae
julioschuambach <julioschuambach.dev@gmail.com>
2024-03-19 22:12:39 -0300

    resource: created initial console application projects for buying and selling operations

---
+++ CryptoTrading/CryptoTrading.Buying/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoTrading.Buying
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
